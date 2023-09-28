94e66f8cd67c75689fdb3ddf002f0b177e638154
juruohejiarui <1466196957@qq.com>
2023-09-28 17:48:16 +0800

    Rename the folder

--- UFUG2601/HW1/50013538-hejiarui/painting/main.cpp
+++ UFUG2601/HW1/50013538-hejiarui/painting/main.cpp
@@ -18,20 +18,21 @@ int main() {
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++) mp[i][j] = '.';
     while (q--) {
-        int x, y, mx = 0, my = 0; char c, d;
+        int x, y, mx = 0, my = 0; char col, direction;
         scanf("%d%d", &x, &y);
-        c = getchar();
-        while (c < 'a' || c > 'z') c = getchar();
-        d = getchar();
-        while (d != 'L' && d != 'U' && d != 'R' && d != 'D') d = getchar();
-        switch(d) {
+        col = getchar();
+        while (col < 'a' || col > 'z') col = getchar();
+        direction = getchar();
+        while (direction != 'L' && direction != 'U' && direction != 'R' && direction != 'D') 
+            direction = getchar();
+        switch(direction) {
             case 'U': mx = -1; break;
             case 'D': mx = 1; break;
             case 'L': my = -1; break;
             case 'R': my = 1; break;
         }
         for (; 1 <= x && x <= n && 1 <= y && y <= m; x += mx, y += my)
-            mp[x][y] = c;
+            mp[x][y] = col;
     }
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) putchar(mp[i][j]);
