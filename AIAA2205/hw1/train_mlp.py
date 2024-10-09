398b4abb99d1975e9f5fd190e23818279547efc1
juruohejiarui <1466196957@qq.com>
2024-10-09 15:39:31 +0800

    TEMP

--- AIAA2205/hw1/train_mlp.py
+++ AIAA2205/hw1/train_mlp.py
@@ -3,14 +3,18 @@ import os
 import pickle
 import argparse
 import sys
+import sklearn.cross_decomposition
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.ensemble import GradientBoostingClassifier, HistGradientBoostingClassifier, AdaBoostRegressor, AdaBoostClassifier, VotingClassifier
 from sklearn.model_selection import RandomizedSearchCV
+import sklearn.model_selection
 from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.preprocessing import StandardScaler
+import sklearn
 
 import scipy.stats as sci
+import sklearn.tests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("feat_dir")
@@ -59,21 +63,22 @@ if __name__ == "__main__" :
 	mlp = MLPClassifier(max_iter=500)
 
 	param_dist = {
-		'hidden_layer_sizes': [(100, 50), (200, 50), (300, 50), (100, 50, 25), (200, 50, 25), (300, 50, 25), (400, 50, 25), (800,50,25)],
+		'hidden_layer_sizes': [(800,50,25), (1000, 50, 25)],
 		'activation': ['relu', 'tanh', 'logistic','identity', 'leaky_relu'],
 		'solver': ['adam', 'sgd', 
 			#  'lbfgs'
 			],
-		'alpha': sci.uniform(1e-5, 1e-2),
+		'alpha': sci.uniform(1e-5, 4e-3),
 		'learning_rate': ['constant', 'invscaling', 'adaptive'],
-		'learning_rate_init': [1e-4,1e-3,1e-2,2e-3,2e-3,3e-3,5e-4,3e-4], 
-		'max_iter': [500, 1000],
+		'learning_rate_init': sci.uniform(1e-3 - 1e-5, 1e-3 + 2e-5), 
+		'max_iter': [1000],
 	}
-	random_search = RandomizedSearchCV(mlp, param_distributions=param_dist, n_iter=100, n_jobs=-1)
+	random_search = RandomizedSearchCV(mlp, param_distributions=param_dist, n_iter=100, n_jobs=32 - 1)
 	random_search.fit(X, Y)
 	mlp = random_search.best_estimator_
+	acc = sklearn.model_selection.cross_val_score(mlp, X, Y, cv=5)
 	
 	pickle.dump(mlp, open(args.output_file, 'wb'))
 	pickle.dump(scaler, open('models/scaler', 'wb'))
-	print('Bmlp classifier trained successfully')
+	print('Bmlp classifier trained successfully, acc={0}' % (acc))
 	print(f'best choice: {random_search.best_params_}')
