d1c49685f8f1c55de76f56dbfd71b3a37dd870ea
juruohejiarui <1466196957@qq.com>
2024-12-15 14:27:02 +0800

    Update DSAA2043 Project

--- DSAA2043/Proj/puzzle8.py
+++ DSAA2043/Proj/puzzle8.py
@@ -1,30 +1,38 @@
 import heapq
 import copy
 import argparse
+import time
 
+# trg = [(1, 1), (0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (2, 1), (2, 0), (1, 0)]
 trg = [(2, 2), (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1)]
+mvVec = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 class State :
-    def __init__(self, mtx : list) :
-        self.mtx = mtx.copy()
-    def dis(self) -> int :
-        global trg
-        ans = 0
+    def _calcHsPred(self) :
+        self.hs = self.pred = 0
         for i in range(3) :
             for j in range(3) :
+                self.hs = self.hs * 10 + self.mtx[i][j]
                 trgX, trgY = trg[self.mtx[i][j]]
-                ans += abs(i - trgX) + abs(j - trgY)
-        return ans
+                self.pred += abs(i - trgX) + abs(j - trgY)
+    def __init__(self, mtx) :
+        if isinstance(mtx, list) :
+            self.mtx = mtx.copy()        
+            self._calcHsPred()
+        elif isinstance(mtx, State) :
+            self.mtx = mtx.mtx.copy()
+            self.hs, self.pred = mtx.hs, mtx.pred
+            
     def __getitem__(self, x) -> int :
         return self.mtx[x[0]][x[1]]
     def __setitem__(self, x, y) :
         self.mtx[x[0]][x[1]] = y
     def __lt__(self, other) -> bool :
-        return self.toInt() < other.toInt()
+        return self.hs < other.hs
     def __eq__(self, other) -> bool :
         if not isinstance(other, State) : return False
         else : 
-            return self.toInt() == other.toInt()
+            return self.hs == other.hs
     
     def find0(self) :
         for i in range(3) :
@@ -33,26 +41,27 @@ class State :
         return (-1, -1)
     def swap(self, p1 : tuple[int, int], p2 : tuple[int, int]) :
         self.mtx[p1[0]][p1[1]], self.mtx[p2[0]][p2[1]] = self.mtx[p2[0]][p2[1]], self.mtx[p1[0]][p1[1]]
-    def toInt(self) :
-        val = 0
-        for i in range(3) :
-            for j in range(3) :
-                val = val * 10 + self.mtx[i][j]
-        return val
+        self._calcHsPred()
     def print(self, outputFile) :
-        for i in range(3) :
-            for j in range(3) :
-                outputFile.write(f"{self.mtx[i][j] if self.mtx[i][j] != 9 else 0} ")
-            outputFile.write('\n')
+        if isinstance(outputFile, None) :
+            for i in range(3) :
+                for j in range(3) :
+                    print(f"{self.mtx[i][j] if self.mtx[i][j] != 9 else 0} ", end='')
+                print('')
+        else :
+            for i in range(3) :
+                for j in range(3) :
+                    outputFile.write(f"{self.mtx[i][j] if self.mtx[i][j] != 9 else 0} ")
+                outputFile.write('\n')
     def isFinish(self) :
-        return self.dis() == 0
+        return self.pred == 0
             
 def astar(initState : list, outputPath : str) :
     vis = {}
     initState : State = State(initState)
-    que : list[tuple[int, State]] = [(initState.dis(), initState)]
+    que : list[tuple[int, State]] = [(initState.pred, initState)]
     preState = {}
-    vis[initState.toInt()] = 0
+    vis[initState.hs] = 0
     finishState : State = None
     finDis = -1
     while len(que) :
@@ -62,38 +71,39 @@ def astar(initState : list, outputPath : str) :
             finDis = dis
             break
         x, y = u.find0()
-        dis -= u.dis()
-        # print(f"dis={dis}")
-        # u.print()
-        for dx in range(-1, 2) :
-            for dy in range(-1, 2) :
-                if abs(dx) + abs(dy) != 1 : continue
-                x1, y1 = x + dx, y + dy
-                if x1 < 0 or y1 < 0 or x1 > 2 or y1 > 2 :
-                    continue
-                v = copy.deepcopy(u)
-                v.swap((x, y), (x1, y1))
-                hsV = v.toInt()
-                if hsV not in vis or vis[hsV] > dis + 1:
-                    vis[hsV] = dis + 1
-                    preState[hsV] = u
-                    heapq.heappush(que, (v.dis() + dis + 1, v))
-    outputFile = open(outputPath, "w")
-    # print solution
-    if finDis == -1 :
-        outputFile.write("UNSOLVABLE\n")
+        dis -= u.pred
+        for dx, dy in mvVec :
+            x1, y1 = x + dx, y + dy
+            if x1 < 0 or y1 < 0 or x1 > 2 or y1 > 2 :
+                continue
+            v = copy.deepcopy(u)
+            v.swap((x, y), (x1, y1))
+            hsV = v.hs
+            if hsV not in vis or vis[hsV] > dis + 1:
+                vis[hsV] = dis + 1
+                preState[hsV] = u
+                heapq.heappush(que, (v.pred + dis + 1, v))
+    if outputPath == None :
+        if finDis == -1 :
+            print("-1")
+        else :
+            print(finDis)
     else :
-        initState.print(outputFile)
-        # print(finDis)
-        ansStk : list[State] = []
-        state : State = finishState
-        while True :
-            ansStk.append(state)
-            state = preState[state.toInt()]
-            if state == initState : break
-        while len(ansStk) > 0 :
-            outputFile.write("\n")
-            ansStk.pop().print(outputFile)
+        outputFile = open(outputPath, 'w')
+        # print solution
+        if finDis == -1 :
+            outputFile.write("UNSOLVABLE\n")
+        else :
+            initState.print(outputFile)
+            # print(finDis)
+            ansStk : list[State] = []
+            state : State = finishState
+            while state != initState :
+                ansStk.append(state)
+                state = preState[state.hs]
+            while len(ansStk) > 0 :
+                outputFile.write("\n")
+                ansStk.pop().print(outputFile)
 
 
 if __name__ == "__main__" :
@@ -105,7 +115,9 @@ if __name__ == "__main__" :
     #         ans[i][j] = seq % 10 
     #         seq //= 10
     # # print(ans)
-    # astar(ans)
+    # # st = time.time()
+    # astar(ans, None)
+
     
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", type=str)
@@ -119,4 +131,6 @@ if __name__ == "__main__" :
     for i in range(3) :
         initState[i] = list(map(int, inputFile.readline().split()))
     astar(initState, outputFile)
+    
+    # print(time.time() - st)
         
