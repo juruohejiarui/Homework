8bbe27de9681dc14fa36e1cf6ab05867d00888a7
juruohejiarui <1466196957@qq.com>
2025-05-08 01:15:35 +0800

    Start DSAA2011 Final Project

--- DSAA2011/Proj/visualize.py
+++ DSAA2011/Proj/visualize.py
@@ -7,22 +7,30 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import data
 
-def plot(x, y, dim_input, dim_target = 3) :
-    tsne = sk.manifold.TSNE(n_components=dim_target, random_state=42)
-    x_tsne = tsne.fit_transform(x)
-    print(x_tsne[0 : 5])
+def plot(x, y, dim_target = 3) :
+    tsne = sk.manifold.TSNE(n_components=dim_target)
+    x_tsne : np.ndarray = tsne.fit_transform(x)
     print(x_tsne.shape)
 
-    fig = plt.figure(figsize=(8, 8))
-    ax = Axes3D(fig)
+    if dim_target == 2 :
+        plt.figure()
+        plt.title(f"t-SNE 2")
+        plt.scatter(x_tsne[:, 0], x_tsne[:, 1], c=plt.cm.Set1(y / 6), s=2)
 
-    ax.scatter(x_tsne[:, 0], x_tsne[:, 1], x_tsne[:, 2], 
-               c=matplotlib.colormaps.get_cmap("plasma")(y / 6.0), s=2)
+    elif dim_target == 3 :
 
-    # plt.legend()
+        fig = plt.figure()
+        ax = Axes3D(fig)
+
+        ax.set_title("t-SNE 3")
+        ax.scatter(x_tsne[:, 0], x_tsne[:, 1], x_tsne[:, 2], 
+                c=plt.cm.Set1(y / 6))
+
     plt.show()
 
 if __name__ == "__main__" :
     X, y = data.load_data("./Data/train", "train")
     print(X.shape, y.shape)
+
+
     plot(X, y, 3)
--- DSAA2011/note-decisiontree.md
+++ DSAA2011/note-decisiontree.md
@@ -1,2 +1,31 @@
 # Decision Tree
 
+## Process of Building a Decision Tree
+
+For each split of a dataset $M$, do the following operations repeatedly:
+
+- For each feature, try to split $M$ according to it and calculate the the classification error of split.
+- Choose the feature with lowest classification error and split the data.
+- Check whether should stop :
+  - **Purity**: All data in one node belongs to one class 同一节点的所有数据的分类相同
+  - **Max Depth**: Meet the limitation of depth of tree 达到了决策树的深度限制
+  - **Min Samples**: There are too few data remain 当前剩余的数据个数过少 $|M|<\delta$ 
+
+Suitable stop conditions make the tree managable and prevents overfitting.
+
+## Pros and Cons
+
+**Pros**:
+
+- Handle large datasets 容易处理较大的数据集
+- handle mixed predictors (continuous, discrete, qualitative) 容易混合不同的预测器（模型）
+- High robustness: 
+  - Can ignore redundant variables 可以忽略冗余的变量
+  - Can easily handle missing data 可以处理缺失的数据
+- Easy to interpret if small 规模较小的时候容易进行解释
+
+**Cons**:
+
+- Prediction preformance is poor 预测表现能力弱
+- Does not generalize well 泛化性较弱
+- Large trees are hard to interpret 规模较大的时候可解释性较差
