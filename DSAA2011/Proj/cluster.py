9dd3ec9814650a1eda4f1d4fa04ee60d113dfacf
juruohejiarui <1466196957@qq.com>
2025-05-16 22:55:42 +0800

    TEMP

--- DSAA2011/Proj/cluster.py
+++ DSAA2011/Proj/cluster.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, fowlkes_mallows_score, rand_score, adjusted_rand_score
 import numpy as np
 import data
 import visualize
@@ -20,6 +21,22 @@ def hierarchical(X : np.ndarray, n_clusters: int) -> tuple :
 
 	return ag.labels_, ag.children_
 
+def metric(name : str, X : np.ndarray, y : np.ndarray, labels : np.ndarray, centers : np.ndarray) :
+	print(f"metric of {name}")
+
+	# fowlkes_mallows score
+	print(f"fowlkes_mallows score : {fowlkes_mallows_score(y, labels)}")
+
+	# silhouette score
+	print(f"silhouette score : {silhouette_score(X, labels)}")
+
+	# rand score
+	print(f"rand score : {rand_score(y, labels)}")
+
+	# adjusted rand score
+	print(f"adjusted rand score : {adjusted_rand_score(y, labels)}")
+
+
 if __name__ == "__main__" :
 	X, y = data.load_data("./Data/train", "train")
 
@@ -27,7 +44,9 @@ if __name__ == "__main__" :
 
 	labels_dem, centers_dem = kmeans(x_dem, 6, 20)
 	# cluster using the orginal data
-	labels_org, children_org = kmeans(X, 6, 20)
+	labels_org, centers_org = kmeans(X, 6, 20)
 	# create two figures at the same time
-	visualize.plot([x_dem, x_dem, x_dem], [labels_dem, labels_org, y], 2, need_dimension_reduction=False)
+	# visualize.plot([x_dem, x_dem, x_dem], [labels_dem, labels_org, y], dim_target=3, need_dimension_reduction=False)
 	
+	metric("kmeans orginal", X, y, labels_org, centers_org)
+	metric("kmeans dimension reduction", x_dem, y, labels_dem, centers_dem)
