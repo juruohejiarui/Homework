d95a97f871d765e6d70372430f89113aadeb5672
juruohejiarui <1466196957@qq.com>
2025-05-18 18:01:52 +0800

    rename dataset

--- DSAA2011/Proj/exploration.py
+++ DSAA2011/Proj/exploration.py
@@ -8,39 +8,6 @@ import data
 
 fig = plt.figure(figsize=(10, 10))
 
-def ridge(X_train, y_train, X_test, y_test) -> tuple[RidgeClassifier, float] :
-
-	search = GridSearchCV(
-		RidgeClassifier(),
-		param_grid={
-			'alpha': [0.1, 1, 10, 100],
-			'max_iter': [1000, 2000]
-		},
-		cv=5,
-		n_jobs=-1,
-		scoring='f1_macro'
-	)
-	# Initialize the model
-	
-	# Start the timer
-	start_time = time()
-	
-	# Fit the model on the training data
-	search.fit(X_train, y_train)
-	
-	# Stop the timer
-	end_time = time()
-	
-	# Calculate time cost
-	time_cost = end_time - start_time
-	
-	bst = search.best_estimator_
-	# Print the best parameters
-	print("Best parameters:", search.best_params_)
-	evaluate("Ridge Classifier", bst, X_test, y_test, time_cost, eval_roc=False)
-		
-	return bst, time_cost
-
 def logistic(X_train, y_train, X_test, y_test) -> tuple[LogisticRegression, float] :
 	search = GridSearchCV(
 		LogisticRegression(random_state=114),
@@ -70,5 +37,6 @@ if __name__ == "__main__":
 	X_test, y_test = data.load_data("./data/test", "test")
 	# ridge(X_train, y_train, X_test, y_test)
 	logistic(X_train, y_train, X_test, y_test)
-
+	
+	plt.show()
 	# plt.savefig("grid_search.png")
